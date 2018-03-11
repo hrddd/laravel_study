718beeeffbf35d3cbe40636fe3e8c5ee3f6363b2
田田田 <nuhhunnuhhun@gmail.com>
2018-03-12 02:02:43 +0900

    add database and migration file

---
+++ database/migrations/2018_03_11_163618_create_chapters_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChaptersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chapters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('video_id');
+            $table->integer('index');
+            $table->string('text');
+            $table->time('start_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chapters');
+    }
+}
---
+++ database/migrations/2018_03_11_163710_create_questions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('video_id');
+            $table->integer('chapter_id');
+            $table->integer('user_id');
+            $table->string('text');
+            $table->time('time');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
